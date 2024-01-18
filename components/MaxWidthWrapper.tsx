a73fd98129b77ad00b39cd15887e0108fe15d01d
vishav-singla <vishavsinglaofficial@gmail.com>
2024-01-18 17:48:48 +0530

    home page and navbar almost done

--- components/MaxWidthWrapper.tsx
+++ components/MaxWidthWrapper.tsx
@@ -1,17 +1,22 @@
-import { cn } from '@/lib/utils';
-import { ReactNode } from 'react';
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
 
-interface MaxWidthWrapperProps {
-  className?: string;
-  children: ReactNode;
-}
-
-const MaxWidthWrapper = ({ className, children }: MaxWidthWrapperProps) => {
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: ReactNode
+}) => {
   return (
-    <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20")}>
+    <div
+      className={cn(
+        'mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
+        className
+      )}>
       {children}
     </div>
-  );
-};
+  )
+}
 
-export default MaxWidthWrapper;
+export default MaxWidthWrapper
